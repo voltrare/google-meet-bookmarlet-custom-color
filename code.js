f7e05c14f1e9ca03870fe5647447675c4a3e1ce3
voltrare <30526233+voltrare@users.noreply.github.com>
2021-08-18 01:41:05 +0800

    Update script
    toolbar height set to 69px

--- code.js
+++ code.js
@@ -1,23 +1,20 @@
 // paste this code in console (ctrl + shift + i)
 // only works in google meet
 
-// verify website
-var currentLocation = window.location.hostname;
-if (currentLocation == 'meet.google.com') {
-  var color = prompt("What color for background?", 'teal');
-  var gradient1 = prompt("What color for gradient 'middle'?", 'beige');
-  var gradient2 = prompt("What color for gradient 'edge'?", 'teal');
-
-  // "ow3" is the id for the "background" (not the body tag)
-  document.getElementById('ow3').style.background = color;
-
-  // "".xPh1xb.P9KVBf" is the class for the toolbar section
-  document.querySelector('.xPh1xb.P9KVBf').id = 'customtoolbarcolor';
-  document.getElementById('customtoolbarcolor').style.background = "linear-gradient(to right, " + gradient2 + "," + gradient1 + ", " + gradient2 + ")";
-} else {
-  if (confirm("This is not google meet but you can change some color too 'not perfect'")) { 
-    document.body.style.color= prompt("What color for text?",'white');
-    document.body.style.background= prompt("What color for background?",'black');
+(function () {
+  var currentLocation = window.location.hostname;
+  if (currentLocation == 'meet.google.com') {
+    var backgroundcolorsolid = prompt("What color for background?", 'teal');
+    var toolbargradientmiddle = prompt("What color for toolbar gradient 'middle'?", 'beige');
+    var toolbargradientedge = prompt("What color for toolbar gradient 'edge'?", 'teal');
+    document.getElementById('ow3').style.background = 'backgroundcolorsolid';
+    document.querySelector('.xPh1xb.P9KVBf').id = 'toolbarcolor';
+    document.getElementById('toolbarcolor').style.background = 'linear-gradient(to right, " + toolbargradientedge + "," + toolbargradientmiddle + ", " + toolbargradientedge + ")';
+    document.getElementById('toolbarcolor').style.height = '69px';
   } else {
+    if (confirm("This is not google meet but you can change some color too 'not perfect'")) {
+      document.body.style.color = prompt("What color for text?", 'white');
+      document.body.style.background = prompt("What color for background?", 'black');
+    } else { }
   }
-}
+})();
--- other-scripts.js
+++ other-scripts.js
@@ -1,6 +1,6 @@
 //black and white @ https://stackoverflow.com/questions/38833212/how-can-i-change-the-font-color-of-all-texts-of-your-website-using-javascript/38833684
-var all = document.getElementsByTagName("*");
 
+var all = document.getElementsByTagName("*");
 for (var i=0, max=all.length; i < max; i++) {
 all[i].style.background = "black";
 all[i].style.color = "white";
